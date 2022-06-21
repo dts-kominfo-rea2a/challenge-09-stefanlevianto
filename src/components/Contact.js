4b1450a1d4e353a8b0957fcbcc9ac983250b494c
stefanlevianto <stefanlevianto@gmail.com>
2022-06-21 18:11:45 +0700

    commit 1

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,19 @@
 // Terima props lalu tampilkan dalam Contact component
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
-import React from 'react';
+import React from "react";
+import "./Contact.css";
 
-const Contact = () => {
-    return (
-        <></>
-    )
-}
+const Contact = (props) => {
+  return (
+    <div className="container">
+      <img src={props.data.photo} alt="Pas Foto"></img>
+      <div className="biodata">
+        <h1>{props.data.name}</h1>
+        <p>{props.data.phone}</p>
+        <p>{props.data.email}</p>
+      </div>
+    </div>
+  );
+};
 
 export default Contact;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,14 @@
 // Kalian bisa menambahkan CSS di src/components/Header.css
-import React from 'react';
-
+import React from "react";
+import "./Header.css";
 const Header = () => {
-    return (
-        <></>
-    )
-}
+  return (
+    <div>
+      <h1>Call A Friend </h1>
+      <p>Your Friendly Contact App</p>
+      <h2>************************************************************</h2>
+    </div>
+  );
+};
 
 export default Header;
